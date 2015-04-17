4cf9d0011403b0174924b26eb35af0167a20a14b
Simon Tremblay <simon.tremblay@cspq.gouv.qc.ca>
2015-04-17 14:13:29 -0400

    Installation de Apache et activation des modules PHP et ses extensions MapServer gcc, make, Perl 5 Compatible Regular Expression Library

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,10 +6,71 @@
 #
 # Actions:
 #
-# Requires: see Modulefile
+# Requires: see metadata.json
 #
 # Sample Usage:
 #
 class igo {
+
+  $apacheDocRootPath = '/var/www'
+
+  class { 'apache': 
+    mpm_module => 'prefork',
+    default_vhost => false,
+    user         => 'vagrant',
+    group        => 'vagrant'
+  }
+  class {'::apache::mod::php': }
+  class {'::apache::mod::rewrite': }
+  class {'::apache::mod::cgi': }
+
+apache::vhost { 'projeqtorquebec':
+  vhost_name       => '*',
+  port             => '80',
+  docroot          => $apacheDocRootPath,
+  docroot_owner    => 'vagrant',
+  docroot_group    => 'vagrant',
+  directories      => 
+  [
+    {
+      'path'     => $apacheDocRootPath,
+      'provider' => 'directory',
+      'options'  => ['Indexes','FollowSymLinks','MultiViews'],
+      'deny'     => 'from all',
+      'allow_override' => 'All'
+    },
+  ],
+}
+
+# TODO: A confirmer.
+file { '/var/www':
+  ensure => 'link',
+  target => '/vagrant',
+  force  => true
+}
+
+  package {'cgi-mapserver':
+    ensure => '6.4.1-2'
+  }
+
+  package {'mapserver-bin':
+    ensure => '6.4.1-2'
+  }
+
+  package {'gdal-bin':
+    ensure => '6.4.1-2'
+  }
+
+  package {'gcc': }
+  package {'make': }
+  package {'libpcre3-dev': }
+  
+  class { 'php': }
+  class { 'php::dev': }
+
+  class { 'php::extension::curl': }
+  class { 'php::extension::intl': }
+  class { 'php::extension::mapscript': }
+  class { 'php::extension::pgsql': }  
 
 }
